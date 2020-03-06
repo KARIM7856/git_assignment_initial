b43e3b25dd682dc8f23a49ebcfb81fe9575bbadd
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2020-03-06 22:22:39 +0200

    Update SummationSeries.java

--- src/SummationSeries.java
+++ src/SummationSeries.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package summationseries;
 
 
 public class SummationSeries implements ISubscriber 
